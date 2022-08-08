94f192bc25bfa7a4f4c3d56f37ae0b960a3a9e79
sid_upadhyayula <s.upadhyayula1@ufl.edu>
2022-08-08 15:07:15 -0400

    Final Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import * as axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [joke, setJoke] = useState('');
+
+  const getJoke = () => {
+    axios.get("https://v2.jokeapi.dev/joke/Any").then(
+      (response) => {
+        console.log(response);
+        setJoke(response.data.joke);
+      }
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getJoke}>Random Joke</button>
+      <h2>{joke}</h2>
     </div>
   );
 }
